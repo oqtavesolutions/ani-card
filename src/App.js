be420be85faec0c9ddee75d30b051248c5608839
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-11-16 16:17:46 -0500

    Anicard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://api.jikan.moe/v3/search/character?q=Naruto&page=1"
+      )
+      .then((response) => {
+        console.log(response.data);
+        // axios
+        //   .get(
+        //     `https://cors-anywhere.herokuapp.com/${response.data.results[0].url}`
+        //   )
+        //   .then((response) => console.log(response.data));
+      });
+  }
+  render() {
+    return <div>This is a class based</div>;
+  }
 }
-
-export default App;
---
+++ src/utils/chars.js
@@ -0,0 +1,1002 @@
+export const characters = [
+  {
+    id: "1",
+    name: "Lamperouge, Lelouch",
+    url: "/417/Lelouch_Lamperouge",
+  },
+  {
+    id: "2",
+    name: "Lawliet, L",
+    url: "/71/L_Lawliet",
+  },
+  {
+    id: "3",
+    name: "Monkey D., Luffy",
+    url: "/40/Luffy_Monkey_D",
+  },
+  {
+    id: "4",
+    name: "Levi",
+    url: "/45627/Levi",
+  },
+  {
+    id: "5",
+    name: "Yagami, Light",
+    url: "/80/Light_Yagami",
+  },
+  {
+    id: "6",
+    name: "Elric, Edward",
+    url: "/11/Edward_Elric",
+  },
+  {
+    id: "7",
+    name: "Roronoa, Zoro",
+    url: "/62/Zoro_Roronoa",
+  },
+  {
+    id: "8",
+    name: "Okabe, Rintarou",
+    url: "/35252/Rintarou_Okabe",
+  },
+  {
+    id: "9",
+    name: "Uzumaki, Naruto",
+    url: "/17/Naruto_Uzumaki",
+  },
+  {
+    id: "10",
+    name: "Zoldyck, Killua",
+    url: "/27/Killua_Zoldyck",
+  },
+  {
+    id: "11",
+    name: "Sakata, Gintoki",
+    url: "/672/Gintoki_Sakata",
+  },
+  {
+    id: "12",
+    name: "Uchiha, Itachi",
+    url: "/14/Itachi_Uchiha",
+  },
+  {
+    id: "13",
+    name: "Hikigaya, Hachiman",
+    url: "/67065/Hachiman_Hikigaya",
+  },
+  {
+    id: "14",
+    name: "Makise, Kurisu",
+    url: "/34470/Kurisu_Makise",
+  },
+  {
+    id: "15",
+    name: "Kirigaya, Kazuto",
+    url: "/36765/Kazuto_Kirigaya",
+  },
+  {
+    id: "16",
+    name: "Rem",
+    url: "/118763/Rem",
+  },
+  {
+    id: "17",
+    name: "Guts",
+    url: "/422/Guts",
+  },
+  {
+    id: "18",
+    name: "Saitama",
+    url: "/73935/Saitama",
+  },
+  {
+    id: "19",
+    name: "Kaneki, Ken",
+    url: "/87275/Ken_Kaneki",
+  },
+  {
+    id: "20",
+    name: "Ackerman, Mikasa",
+    url: "/40881/Mikasa_Ackerman",
+  },
+  {
+    id: "21",
+    name: "Hatake, Kakashi",
+    url: "/85/Kakashi_Hatake",
+  },
+  {
+    id: "22",
+    name: "Yato",
+    url: "/84677/Yato",
+  },
+  {
+    id: "23",
+    name: "Spiegel, Spike",
+    url: "/1/Spike_Spiegel",
+  },
+  {
+    id: "24",
+    name: "Megumin",
+    url: "/117225/Megumin",
+  },
+  {
+    id: "25",
+    name: "Senjougahara, Hitagi",
+    url: "/22037/Hitagi_Senjougahara",
+  },
+  {
+    id: "26",
+    name: "Son, Gokuu",
+    url: "/246/Gokuu_Son",
+  },
+  {
+    id: "27",
+    name: "Aisaka, Taiga",
+    url: "/12064/Taiga_Aisaka",
+  },
+  {
+    id: "28",
+    name: "Gasai, Yuno",
+    url: "/4963/Yuno_Gasai",
+  },
+  {
+    id: "29",
+    name: "Morow, Hisoka",
+    url: "/31/Hisoka_Morow",
+  },
+  {
+    id: "30",
+    name: "Kamina",
+    url: "/2075/Kamina",
+  },
+  {
+    id: "31",
+    name: "Uchiha, Sasuke",
+    url: "/13/Sasuke_Uchiha",
+  },
+  {
+    id: "32",
+    name: "Mustang, Roy",
+    url: "/68/Roy_Mustang",
+  },
+  {
+    id: "33",
+    name: "Joestar, Joseph",
+    url: "/6356/Joseph_Joestar",
+  },
+  {
+    id: "34",
+    name: "Saber",
+    url: "/497/Saber",
+  },
+  {
+    id: "35",
+    name: "Oshino, Shinobu",
+    url: "/23602/Shinobu_Oshino",
+  },
+  {
+    id: "36",
+    name: "Emilia",
+    url: "/118737/Emilia",
+  },
+  {
+    id: "37",
+    name: "Yuuki, Asuna",
+    url: "/36828/Asuna_Yuuki",
+  },
+  {
+    id: "38",
+    name: "Yukinoshita, Yukino",
+    url: "/67067/Yukino_Yukinoshita",
+  },
+  {
+    id: "39",
+    name: "Holo",
+    url: "/7373/Holo",
+  },
+  {
+    id: "40",
+    name: "Kurosaki, Ichigo",
+    url: "/5/Ichigo_Kurosaki",
+  },
+  {
+    id: "41",
+    name: "Alucard",
+    url: "/601/Alucard",
+  },
+  {
+    id: "42",
+    name: "Zero Two",
+    url: "/155679/Zero_Two",
+  },
+  {
+    id: "43",
+    name: "Ishigami, Yuu",
+    url: "/143185/Yuu_Ishigami",
+  },
+  {
+    id: "44",
+    name: "Onizuka, Eikichi",
+    url: "/434/Eikichi_Onizuka",
+  },
+  {
+    id: "45",
+    name: "Orihara, Izaya",
+    url: "/24596/Izaya_Orihara",
+  },
+  {
+    id: "46",
+    name: "Araragi, Koyomi",
+    url: "/22036/Koyomi_Araragi",
+  },
+  {
+    id: "47",
+    name: "Sanji",
+    url: "/305/Sanji",
+  },
+  {
+    id: "48",
+    name: "Yeager, Eren",
+    url: "/40882/Eren_Yeager",
+  },
+  {
+    id: "49",
+    name: "Dragneel, Natsu",
+    url: "/5187/Natsu_Dragneel",
+  },
+  {
+    id: "50",
+    name: "Michaelis, Sebastian",
+    url: "/10863/Sebastian_Michaelis",
+  },
+  {
+    id: "51",
+    name: "Akemi, Homura",
+    url: "/38005/Homura_Akemi",
+  },
+  {
+    id: "52",
+    name: "Midoriya, Izuku",
+    url: "/117909/Izuku_Midoriya",
+  },
+  {
+    id: "53",
+    name: "C.C.",
+    url: "/1111/CC",
+  },
+  {
+    id: "54",
+    name: "Nico, Robin",
+    url: "/61/Robin_Nico",
+  },
+  {
+    id: "55",
+    name: "Todoroki, Shouto",
+    url: "/118489/Shouto_Todoroki",
+  },
+  {
+    id: "56",
+    name: "Miyazono, Kaori",
+    url: "/69411/Kaori_Miyazono",
+  },
+  {
+    id: "57",
+    name: "Takanashi, Rikka",
+    url: "/65865/Rikka_Takanashi",
+  },
+  {
+    id: "58",
+    name: "Izumi, Konata",
+    url: "/2169/Konata_Izumi",
+  },
+  {
+    id: "59",
+    name: "Vegeta",
+    url: "/913/Vegeta",
+  },
+  {
+    id: "60",
+    name: "Dazai, Osamu",
+    url: "/125056/Osamu_Dazai",
+  },
+  {
+    id: "61",
+    name: "Souryuu, Asuka Langley",
+    url: "/94/Asuka_Langley_Souryuu",
+  },
+  {
+    id: "62",
+    name: "Sakurajima, Mai",
+    url: "/118739/Mai_Sakurajima",
+  },
+  {
+    id: "63",
+    name: "Oreki, Houtarou",
+    url: "/55131/Houtarou_Oreki",
+  },
+  {
+    id: "64",
+    name: "Reigen, Arataka",
+    url: "/109931/Arataka_Reigen",
+  },
+  {
+    id: "65",
+    name: "Freecss, Gon",
+    url: "/30/Gon_Freecss",
+  },
+  {
+    id: "66",
+    name: "Tokisaki, Kurumi",
+    url: "/70069/Kurumi_Tokisaki",
+  },
+  {
+    id: "67",
+    name: "Heiwajima, Shizuo",
+    url: "/24595/Shizuo_Heiwajima",
+  },
+  {
+    id: "68",
+    name: "Scarlet, Erza",
+    url: "/5189/Erza_Scarlet",
+  },
+  {
+    id: "69",
+    name: "Usui, Takumi",
+    url: "/14523/Takumi_Usui",
+  },
+  {
+    id: "70",
+    name: "Bakugou, Katsuki",
+    url: "/117911/Katsuki_Bakugou",
+  },
+  {
+    id: "71",
+    name: "Hei",
+    url: "/2160/Hei",
+  },
+  {
+    id: "72",
+    name: "Suzumiya, Haruhi",
+    url: "/251/Haruhi_Suzumiya",
+  },
+  {
+    id: "73",
+    name: "Evergarden, Violet",
+    url: "/141354/Violet_Evergarden",
+  },
+  {
+    id: "74",
+    name: "Misaka, Mikoto",
+    url: "/13701/Mikoto_Misaka",
+  },
+  {
+    id: "75",
+    name: "Revy",
+    url: "/458/Revy",
+  },
+  {
+    id: "76",
+    name: "Toosaka, Rin",
+    url: "/498/Rin_Toosaka",
+  },
+  {
+    id: "77",
+    name: "Kusanagi, Motoko",
+    url: "/1795/Motoko_Kusanagi",
+  },
+  {
+    id: "78",
+    name: "Fujiwara, Chika",
+    url: "/140810/Chika_Fujiwara",
+  },
+  {
+    id: "79",
+    name: "Shiina, Mashiro",
+    url: "/61371/Mashiro_Shiina",
+  },
+  {
+    id: "80",
+    name: "Trafalgar, Law",
+    url: "/13767/Law_Trafalgar",
+  },
+  {
+    id: "81",
+    name: "Yukihira, Souma",
+    url: "/75216/Souma_Yukihira",
+  },
+  {
+    id: "82",
+    name: "Satou, Kazuma",
+    url: "/117221/Kazuma_Satou",
+  },
+  {
+    id: "83",
+    name: "Gremory, Rias",
+    url: "/50389/Rias_Gremory",
+  },
+  {
+    id: "84",
+    name: "Kuujou, Joutarou",
+    url: "/4003/Joutarou_Kuujou",
+  },
+  {
+    id: "85",
+    name: "Sora",
+    url: "/82523/Sora",
+  },
+  {
+    id: "86",
+    name: "Koro-sensei",
+    url: "/65643/Koro-sensei",
+  },
+  {
+    id: "87",
+    name: "Nishimiya, Shouko",
+    url: "/80243/Shouko_Nishimiya",
+  },
+  {
+    id: "88",
+    name: "Shiro",
+    url: "/82525/Shiro",
+  },
+  {
+    id: "89",
+    name: "Kurapika",
+    url: "/28/Kurapika",
+  },
+  {
+    id: "90",
+    name: "Kirisaki, Chitoge",
+    url: "/48391/Chitoge_Kirisaki",
+  },
+  {
+    id: "91",
+    name: "Lucy",
+    url: "/738/Lucy",
+  },
+  {
+    id: "92",
+    name: "Himura, Kenshin",
+    url: "/147/Kenshin_Himura",
+  },
+  {
+    id: "93",
+    name: "Akabane, Karma",
+    url: "/71933/Karma_Akabane",
+  },
+  {
+    id: "94",
+    name: "Tachibana, Kanade",
+    url: "/22369/Kanade_Tachibana",
+  },
+  {
+    id: "95",
+    name: "Matoi, Ryuuko",
+    url: "/83797/Ryuuko_Matoi",
+  },
+  {
+    id: "96",
+    name: "Nara, Shikamaru",
+    url: "/2007/Shikamaru_Nara",
+  },
+  {
+    id: "97",
+    name: "Gaara",
+    url: "/1662/Gaara",
+  },
+  {
+    id: "98",
+    name: "Gilgamesh",
+    url: "/2514/Gilgamesh",
+  },
+  {
+    id: "99",
+    name: "Emiya, Kiritsugu",
+    url: "/10010/Kiritsugu_Emiya",
+  },
+  {
+    id: "100",
+    name: "Ginko",
+    url: "/425/Ginko",
+  },
+  {
+    id: "101",
+    name: "Vash the Stampede",
+    url: "https:///162/Vash_the_Stampede",
+  },
+  {
+    id: "102",
+    name: "Nami",
+    url: "https:///723/Nami",
+  },
+  {
+    id: "103",
+    name: "Shinomiya, Kaguya",
+    url: "https:///136359/Kaguya_Shinomiya",
+  },
+  {
+    id: "104",
+    name: "Yuigahama, Yui",
+    url: "https:///67069/Yui_Yuigahama",
+  },
+  {
+    id: "105",
+    name: "Ayanami, Rei",
+    url: "https:///86/Rei_Ayanami",
+  },
+  {
+    id: "106",
+    name: "Elric, Alphonse",
+    url: "https:///12/Alphonse_Elric",
+  },
+  {
+    id: "107",
+    name: "Hinata, Shouyou",
+    url: "https:///64769/Shouyou_Hinata",
+  },
+  {
+    id: "108",
+    name: "Natsuki, Subaru",
+    url: "https:///118735/Subaru_Natsuki",
+  },
+  {
+    id: "109",
+    name: "Okumura, Rin",
+    url: "https:///24482/Rin_Okumura",
+  },
+  {
+    id: "110",
+    name: "Kageyama, Shigeo",
+    url: "https:///109929/Shigeo_Kageyama",
+  },
+  {
+    id: "111",
+    name: "Okazaki, Tomoya",
+    url: "https:///4606/Tomoya_Okazaki",
+  },
+  {
+    id: "112",
+    name: "Hyuuga, Hinata",
+    url: "https:///1555/Hinata_Hyuuga",
+  },
+  {
+    id: "113",
+    name: "Walker, Allen",
+    url: "https:///139/Allen_Walker",
+  },
+  {
+    id: "114",
+    name: "Mugen",
+    url: "https:///390/Mugen",
+  },
+  {
+    id: "115",
+    name: "Simon",
+    url: "https:///2257/Simon",
+  },
+  {
+    id: "116",
+    name: "Kamado, Tanjirou",
+    url: "https:///146156/Tanjirou_Kamado",
+  },
+  {
+    id: "117",
+    name: "Kageyama, Tobio",
+    url: "https:///64771/Tobio_Kageyama",
+  },
+  {
+    id: "118",
+    name: "Phantomhive, Ciel",
+    url: "https:///12531/Ciel_Phantomhive",
+  },
+  {
+    id: "119",
+    name: "Accelerator",
+    url: "https:///13917/Accelerator",
+  },
+  {
+    id: "120",
+    name: "Ayanokouji, Kiyotaka",
+    url: "https:///128909/Kiyotaka_Ayanokouji",
+  },
+  {
+    id: "121",
+    name: "Ryuk",
+    url: "https:///75/Ryuk",
+  },
+  {
+    id: "122",
+    name: "Hirasawa, Yui",
+    url: "https:///19565/Yui_Hirasawa",
+  },
+  {
+    id: "123",
+    name: "Katsuragi, Misato",
+    url: "https:///1259/Misato_Katsuragi",
+  },
+  {
+    id: "124",
+    name: "Ikari, Shinji",
+    url: "https:///89/Shinji_Ikari",
+  },
+  {
+    id: "125",
+    name: "Akiyama, Mio",
+    url: "https:///19566/Mio_Akiyama",
+  },
+  {
+    id: "126",
+    name: "Usopp",
+    url: "https:///724/Usopp",
+  },
+  {
+    id: "127",
+    name: "Ban",
+    url: "https:///77605/Ban",
+  },
+  {
+    id: "128",
+    name: "Saiki, Kusuo",
+    url: "https:///90291/Kusuo_Saiki",
+  },
+  {
+    id: "129",
+    name: "Smith, Erwin",
+    url: "https:///46496/Erwin_Smith",
+  },
+  {
+    id: "130",
+    name: "Tony Tony, Chopper",
+    url: "https:///309/Chopper_Tony_Tony",
+  },
+  {
+    id: "131",
+    name: "Yuzuriha, Inori",
+    url: "https:///43280/Inori_Yuzuriha",
+  },
+  {
+    id: "132",
+    name: "Meruem",
+    url: "https:///23277/Meruem",
+  },
+  {
+    id: "133",
+    name: "Death the Kid",
+    url: "https:///8455/Death_the_Kid",
+  },
+  {
+    id: "134",
+    name: "Jiraiya",
+    url: "https:///2423/Jiraiya",
+  },
+  {
+    id: "135",
+    name: "Liebert, Johan",
+    url: "https:///719/Johan_Liebert",
+  },
+  {
+    id: "136",
+    name: "Ichigo",
+    url: "https:///155680/Ichigo",
+  },
+  {
+    id: "137",
+    name: "Kyon",
+    url: "https:///252/Kyon",
+  },
+  {
+    id: "138",
+    name: "Brando, Dio",
+    url: "https:///4004/Dio_Brando",
+  },
+  {
+    id: "139",
+    name: "Askeladd",
+    url: "https:///13020/Askeladd",
+  },
+  {
+    id: "140",
+    name: "Furukawa, Nagisa",
+    url: "https:///4604/Nagisa_Furukawa",
+  },
+  {
+    id: "141",
+    name: "Kuroko, Tetsuya",
+    url: "https:///18769/Tetsuya_Kuroko",
+  },
+  {
+    id: "142",
+    name: "Azusagawa, Sakuta",
+    url: "https:///118733/Sakuta_Azusagawa",
+  },
+  {
+    id: "143",
+    name: "Suzuya, Juuzou",
+    url: "https:///104437/Juuzou_Suzuya",
+  },
+  {
+    id: "144",
+    name: "Namikaze, Minato",
+    url: "https:///2535/Minato_Namikaze",
+  },
+  {
+    id: "145",
+    name: "Valentine, Faye",
+    url: "https:///2/Faye_Valentine",
+  },
+  {
+    id: "146",
+    name: "Meliodas",
+    url: "https:///72921/Meliodas",
+  },
+  {
+    id: "147",
+    name: "Zaraki, Kenpachi",
+    url: "https:///909/Kenpachi_Zaraki",
+  },
+  {
+    id: "148",
+    name: "Kaiki, Deishuu",
+    url: "https:///53641/Deishuu_Kaiki",
+  },
+  {
+    id: "149",
+    name: "Kirishima, Touka",
+    url: "https:///87277/Touka_Kirishima",
+  },
+  {
+    id: "150",
+    name: "Akame",
+    url: "https:///63351/Akame",
+  },
+  {
+    id: "151",
+    name: "Nishinoya, Yuu",
+    url: "/character/67327/Yuu_Nishinoya",
+  },
+  {
+    id: "152",
+    name: "Suou, Tamaki",
+    url: "/character/19/Tamaki_Suou",
+  },
+  {
+    id: "153",
+    name: "Arlert, Armin",
+    url: "/character/46494/Armin_Arlert",
+  },
+  {
+    id: "154",
+    name: "Kagura",
+    url: "/character/674/Kagura",
+  },
+  {
+    id: "155",
+    name: "Shiina, Mayuri",
+    url: "/character/35253/Mayuri_Shiina",
+  },
+  {
+    id: "156",
+    name: "Nakano, Itsuki",
+    url: "/character/161471/Itsuki_Nakano",
+  },
+  {
+    id: "157",
+    name: "Makishima, Shougo",
+    url: "/character/69725/Shougo_Makishima",
+  },
+  {
+    id: "158",
+    name: "Kougami, Shinya",
+    url: "/character/68315/Shinya_Kougami",
+  },
+  {
+    id: "159",
+    name: "Degurechaff, Tanya",
+    url: "/character/137825/Tanya_Degurechaff",
+  },
+  {
+    id: "160",
+    name: "Nagato, Yuki",
+    url: "/character/249/Yuki_Nagato",
+  },
+  {
+    id: "161",
+    name: "Aomine, Daiki",
+    url: "/character/36820/Daiki_Aomine",
+  },
+  {
+    id: "162",
+    name: "Senkuu",
+    url: "/character/148984/Senkuu",
+  },
+  {
+    id: "163",
+    name: "Uchiha, Madara",
+    url: "/character/53901/Madara_Uchiha",
+  },
+  {
+    id: "164",
+    name: "Inaba, Himeko",
+    url: "/character/40197/Himeko_Inaba",
+  },
+  {
+    id: "165",
+    name: "Portgas D., Ace",
+    url: "/character/2072/Ace_Portgas_D",
+  },
+  {
+    id: "166",
+    name: "Hitsugaya, Toushirou",
+    url: "/character/245/Toushirou_Hitsugaya",
+  },
+  {
+    id: "167",
+    name: "Momonga",
+    url: "/character/116281/Momonga",
+  },
+  {
+    id: "168",
+    name: "Kiryuu, Zero",
+    url: "/character/3519/Zero_Kiryuu",
+  },
+  {
+    id: "169",
+    name: "Natsume, Takashi",
+    url: "/character/13783/Takashi_Natsume",
+  },
+  {
+    id: "170",
+    name: "Charlotte, Katakuri",
+    url: "/character/150775/Katakuri_Charlotte",
+  },
+  {
+    id: "171",
+    name: "Kamado, Nezuko",
+    url: "/character/146157/Nezuko_Kamado",
+  },
+  {
+    id: "172",
+    name: "Oikawa, Tooru",
+    url: "/character/71611/Tooru_Oikawa",
+  },
+  {
+    id: "173",
+    name: "Kiryuuin, Satsuki",
+    url: "/character/83799/Satsuki_Kiryuuin",
+  },
+  {
+    id: "174",
+    name: "Komaeda, Nagito",
+    url: "/character/73227/Nagito_Komaeda",
+  },
+  {
+    id: "175",
+    name: "Himejima, Akeno",
+    url: "/character/51347/Akeno_Himejima",
+  },
+  {
+    id: "176",
+    name: "Kira, Yoshikage",
+    url: "/character/12055/Yoshikage_Kira",
+  },
+  {
+    id: "177",
+    name: "Tomoe",
+    url: "/character/18344/Tomoe",
+  },
+  {
+    id: "178",
+    name: "Onodera, Kosaki",
+    url: "/character/52723/Kosaki_Onodera",
+  },
+  {
+    id: "179",
+    name: "All Might",
+    url: "/character/117921/All_Might",
+  },
+  {
+    id: "180",
+    name: "Fullbuster, Gray",
+    url: "/character/4839/Gray_Fullbuster",
+  },
+  {
+    id: "181",
+    name: "Pain",
+    url: "/character/3180/Pain",
+  },
+  {
+    id: "182",
+    name: "Hyoudou, Issei",
+    url: "/character/50387/Issei_Hyoudou",
+  },
+  {
+    id: "183",
+    name: "Hanekawa, Tsubasa",
+    url: "/character/22055/Tsubasa_Hanekawa",
+  },
+  {
+    id: "184",
+    name: "Asada, Shino",
+    url: "/character/55147/Shino_Asada",
+  },
+  {
+    id: "185",
+    name: "Archer",
+    url: "/character/2087/Archer",
+  },
+  {
+    id: "186",
+    name: "Kamui, Kanna",
+    url: "/character/136728/Kanna_Kamui",
+  },
+  {
+    id: "187",
+    name: "Shana",
+    url: "/character/1426/Shana",
+  },
+  {
+    id: "188",
+    name: "Cifer, Ulquiorra",
+    url: "/character/1081/Ulquiorra_Cifer",
+  },
+  {
+    id: "189",
+    name: "Brook",
+    url: "/character/5627/Brook",
+  },
+  {
+    id: "190",
+    name: "Jabami, Yumeko",
+    url: "/character/119413/Yumeko_Jabami",
+  },
+  {
+    id: "191",
+    name: "Iwakura, Lain",
+    url: "/character/2219/Lain_Iwakura",
+  },
+  {
+    id: "192",
+    name: "Shiba, Tatsuya",
+    url: "/character/55743/Tatsuya_Shiba",
+  },
+  {
+    id: "193",
+    name: "Raphtalia",
+    url: "/character/112893/Raphtalia",
+  },
+  {
+    id: "194",
+    name: "Ryougi, Shiki",
+    url: "/character/3105/Shiki_Ryougi",
+  },
+  {
+    id: "195",
+    name: "Aqua",
+    url: "/character/117223/Aqua",
+  },
+  {
+    id: "196",
+    name: "Zeppeli, Gyro",
+    url: "/character/11772/Gyro_Zeppeli",
+  },
+  {
+    id: "197",
+    name: "Kuriyama, Mirai",
+    url: "/character/81751/Mirai_Kuriyama",
+  },
+  {
+    id: "198",
+    name: "Rider",
+    url: "/character/16021/Rider",
+  },
+  {
+    id: "199",
+    name: "Katsuragi, Keima",
+    url: "/character/13468/Keima_Katsuragi",
+  },
+  {
+    id: "200",
+    name: "Tomori, Nao",
+    url: "/character/122211/Nao_Tomori",
+  },
+];
